3d22f50232c43eac58fa58586e1936c07709c934
Joseph <45214455+ScouseTeryaki@users.noreply.github.com>
2021-08-08 23:41:47 +0100

    Reformatted code, optimised, and finished event collector

--- DemoRipper/main.py
+++ DemoRipper/main.py
@@ -4,49 +4,91 @@ import csv
 
 
 def main():
-    finals = check_for_csv('finals.csv')
-    # if not finals:
-    #     print('Collecting Event Finals...')
-    #     url = 'https://www.hltv.org/results?stars=5'
-    #     events_list = get_webpage(url)
-    #     matchers = ['<a href="/matches/']
-    #     finals = get_url_from_html(events_list, matchers, 'href="', '"')
-    #     dump_to_file('finals.csv', finals)
-    #     events = remove_dupe_events('finals.csv')
-    #     dump_to_file('finals.csv', events)
-    length = len(finals)
-    quarter_index = length//4
-    first_quarter = finals[:quarter_index]
-    events = []
-    for event in first_quarter:
+    finals = save_event_finals()
+    events = save_events(finals)
+
+
+def save_event_matches():
+    pass
+
+
+def save_events(finals):
+    events = check_for_csv('events.csv')
+    final_length = len(finals)
+    if not events:
+        events = []
+        print('No events saved!')
+        limiter = int(input('How many events do you want to fetch?'))
+        partial_events = finals[:limiter]
+        dump_events(partial_events, events)
+    else:
+        events_length = len(events)
+        left_events = final_length - events_length
+        if left_events != 0:
+            print(str(events_length) + ' events saved!')
+            print(str(left_events) + ' events left!')
+            limiter = int(input('How many events do you want to fetch?'))
+            limiter += events_length
+            partial_events = finals[events_length:limiter]
+            dump_events(partial_events, events)
+        else:
+            print('All events fetched!')
+    return events
+
+
+def dump_events(partial_events, events):
+    new_events = []
+    for event in partial_events:
         final_match_url = event[0]
         final_match_page = get_webpage(final_match_url)
-        event_name_list = event[1].split("-")
-        event_name = "-".join(event_name_list[-3:])
-        print(event_name)
-        matcher = r'<a href="/events/\d{4}/'
-        events = get_url_from_html(final_match_page, matcher, 'href="', '/')
+        start_pointer = '<a href="/events/'
+        end_pointer = f'/.*?" title="{event[1]}"'
+        middle_pointer = r'\d\d\d\d'
+        prefix_url = 'https://www.hltv.org/results?event='
+        event = get_url_from_html(final_match_page, start_pointer, end_pointer, middle_pointer, prefix_url)
+        new_events.append(event)
+    events += new_events
     dump_to_file('events.csv', events)
-    print(events)
 
 
-def remove_dupe_events(filename):
-    with open(filename, mode='r+', newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        items = list(reader)
-        for i in items:
-            for idx, dupe in enumerate(items):
-                if i == dupe:
-                    items.pop(idx)
-        return items
+def save_event_finals():
+    events = check_for_csv('finals.csv')
+    if not events:
+        print('Collecting Event Finals...')
+        url = 'https://www.hltv.org/results?stars=5'
+        events_list = get_webpage(url)
+        start_pointer = '<a href="'
+        end_pointer = '" class="a-reset">'
+        middle_pointer = '/matches/'
+        prefix_url = 'https://www.hltv.org'
+        final_urls = get_url_from_html(events_list, start_pointer, end_pointer, middle_pointer, prefix_url)
+        event_names = get_event_names_from_html(events_list, '<span class="event-name">', '<')
+        finals = []
+        for idx, final in enumerate(final_urls):
+            tmp = [final, event_names[idx]]
+            finals.append(tmp)
+        events = remove_dupe_events(finals)
+        dump_to_file('finals.csv', events)
+    return events
 
 
+# Remove items with the same event name
+def remove_dupe_events(event_list):
+    for i in event_list:
+        for idx, dupe in enumerate(event_list):
+            if i == dupe:
+                event_list.pop(idx)
+    return event_list
+
+
+# Checks for data within a csv file
+# If no data exists returns False
+# If file doesn't exist returns False
 def check_for_csv(filename):
     try:
         with open(filename, mode='r') as csvfile:
             reader = csv.reader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             reader_list = list(reader)
-            # Doesn't work atm (fix later pls)
             if reader_list:
                 return reader_list
             else:
@@ -55,6 +97,7 @@ def check_for_csv(filename):
         return False
 
 
+# Dumps a list into a csv file
 def dump_to_file(filename, data):
     with open(filename, mode='w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -71,19 +114,21 @@ def get_webpage(url):
     return webpage
 
 
-def get_url_from_html(html, matcher, start_pointer, end_pointer):
-    matching = re.findall(matcher, html)
+# Gets a url from a html element using pointers
+def get_url_from_html(html, start_pointer, end_pointer, middle_pointer, prefix_url):
+    matching = re.findall(rf'(?<={start_pointer}){middle_pointer}.*?(?={end_pointer})', html)
     urls = []
     print(matching)
     for event in matching:
-        temp = []
-        url_suffix = re.search(rf'(?<={start_pointer}).*?(?={end_pointer})', event).group(0)
-        url = "https://www.hltv.org" + url_suffix
-        text_suffix = re.search(r'(?<=\d/).*?(?=")', event).group(0)
-        temp.append(url)
-        temp.append(text_suffix)
-        urls.append(temp)
+        url = prefix_url + event
+        urls.append(url)
     return urls
+
+
+# Collects event names to use to remove duplicate events
+def get_event_names_from_html(html, start_pointer, end_pointer):
+    matching = re.findall(rf'(?<={start_pointer}).*?(?={end_pointer})', html)
+    return matching
 
 
 # Downloads CSGO series in a zip
